e401de228f834a7855ebed2618ffb6bea6eeef50
”Frederik <fred295q@stud.kea.dk>
2023-08-30 10:48:30 +0200

    bar chart

---
+++ script.js
@@ -0,0 +1,19 @@
+const list = document.querySelector("ul");
+const arr = [];
+
+setInterval(randTal, 1000);
+
+function randTal() {
+  let barHeight = Math.floor(Math.random() * 100);
+  const li = document.createElement("li");
+  li.style.setProperty("--height", barHeight);
+  list.appendChild(li);
+  if (arr.length >= 20) {
+    // arr.pop();
+    list.firstElementChild.remove();
+  } else {
+    arr.unshift(barHeight);
+  }
+
+  console.log(arr.length);
+}
